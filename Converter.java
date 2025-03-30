7611b9e0902b35831c97b24dd00eec70e668b815
danjb1125 <danjhenielbringas1125@gmail.com>
2025-03-30 22:21:08 +0800

    Added main function with user input

---
+++ Converter.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Converter {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a meter length to convert into kilometers: ");
+
+        input.close();
+
+    }
+}
