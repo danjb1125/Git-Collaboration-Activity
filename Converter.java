eda85a29644cc76d17cfcbd727eae6ad90a64e2f
Hachishib <jevoncastro14@gmail.com>
2025-03-30 22:24:08 +0800

    Updated Converter.java with main method

--- Converter.java
+++ Converter.java
@@ -4,4 +4,11 @@ public class Converter
     {
         return meters / 1000;
     }
+    
+    public static void main (String[] args) 
+    {
+        double meters = 1000;
+        double kilometers = ConvertMtoKm(meters);
+        System.out.println(meters + " meters is equal to " + kilometers + " kilometers.");
+    }
 }
